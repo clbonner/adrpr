4228851300c1e9592611da05cf922aca7349d97c
CLBonner <clawrence.bonner@gmail.com>
2025-07-15 15:12:30 +0100

    Update adrpr.ps1

--- adrpr.ps1
+++ adrpr.ps1
@@ -1,4 +1,4 @@
-# A script for quickly searchinf Active Directory and resetting the password of a user.
+# A script for quickly searching Active Directory and resetting the password of a user.
 # Handy if you work in an environment where you reset passwords often (for example, a school).
 # Written by C L Bonner, 2025.
 
